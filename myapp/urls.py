7a49af31d1f570fae8120d01f323c5521ac59dba
kazem <k_jebraeeli@yahoo.com>
2024-04-28 09:50:40 +0330

    UPDATE and CREATE approach is applied(POST, GET)

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import student_list, student_detail
+from .views import student_list, student_detail, student_save, student_update
 
 
 urlpatterns = [
     path('students_list/', student_list),
     path('student_details/<pk>', student_detail),
-    
+    path('student_save/', student_save),
+    path('student_update/<pk>', student_update),
     
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -15,3 +15,20 @@ def student_detail(request, pk):
     student = Student.objects.get(id=pk)
     student_serializer = StudentSerializer(student, many=False)
     return Response(student_serializer.data)
+
+@api_view(["POST"])
+def student_save(request):
+    student = StudentSerializer(data=request.data)
+    if student.is_valid():
+        student.save()
+    return Response(student.data)
+
+@api_view(["POST", "GET"])
+def student_update(request, pk):
+    instancee = Student.objects.get(id=pk)
+    student = StudentSerializer(instance=instancee, data=request.data)
+    student_now = StudentSerializer(instancee, many=False)
+    if student.is_valid():
+        student.save()
+    return Response( student_now.data)
+    
